49cb24e351de175c90082c6309fb9d3813a2a8fe
David Cotto <dacotto1984@gmail.com>
2024-06-22 20:18:51 -0400

    Added controls to move the movingCube.  Fixed some logic with collisions

--- src/components/threejs/rapier/RapierScene.tsx
+++ src/components/threejs/rapier/RapierScene.tsx
@@ -1,7 +1,7 @@
 import {Canvas, useFrame} from "@react-three/fiber";
 import {OrbitControls} from "@react-three/drei";
 import {Color, DoubleSide, Mesh, MeshPhysicalMaterial, Vector3} from "three";
-import {Suspense, useRef, useState} from "react";
+import {Suspense, useEffect, useRef, useState} from "react";
 import {Physics, RapierRigidBody, RigidBody} from "@react-three/rapier";
 
 function RapierContext() {
@@ -14,6 +14,8 @@ function RapierContext() {
     const cubeMeshRef = useRef<Mesh>(null);
     const cubeMaterialRef = useRef<MeshPhysicalMaterial>(null);
     const cubeCollisionRef = useRef<Boolean>(false);
+
+    const movement = useRef({w: false, a: false, s: false, d: false});
 
     const moveCube = () => {
         if (cubeRigidBodyRef.current) {
@@ -33,17 +35,78 @@ function RapierContext() {
         }
     }
 
+    useEffect(() => {
+        const handleKeyDown = (event: KeyboardEvent) => {
+            switch (event.key.toLowerCase()) {
+                case "w":
+                    movement.current.w = true;
+                    break;
+                case "a":
+                    movement.current.a = true;
+                    break;
+                case "s":
+                    movement.current.s = true;
+                    break;
+                case "d":
+                    movement.current.d = true;
+                    break;
+                default:
+                    break;
+            }
+        };
+
+        const handleKeyUp = (event: KeyboardEvent) => {
+            switch (event.key.toLowerCase()) {
+                case "w":
+                    movement.current.w = false;
+                    break;
+                case "a":
+                    movement.current.a = false;
+                    break;
+                case "s":
+                    movement.current.s = false;
+                    break;
+                case "d":
+                    movement.current.d = false;
+                    break;
+                default:
+                    break;
+            }
+        };
+
+        window.addEventListener("keydown", handleKeyDown);
+        window.addEventListener("keyup", handleKeyUp);
+
+        // Cleanup the event listeners on component unmount
+        return () => {
+            window.removeEventListener("keydown", handleKeyDown);
+            window.removeEventListener("keyup", handleKeyUp);
+        };
+    }, []);
+
+    const speed = 0.01;
+    const direction = new Vector3(2, 0.5, 2);
+
     useFrame(() => {
 
-        if (moveCubeRigidBodyRef.current) {
-            defaultPosition.x -= 0.005;
-            moveCubeRigidBodyRef.current.setNextKinematicTranslation(defaultPosition);
-        }
+        // if (moveCubeRigidBodyRef.current) {
+        //     defaultPosition.x -= 0.005;
+        //     moveCubeRigidBodyRef.current.setNextKinematicTranslation(defaultPosition);
+        // }
+
 
         const targetColor = cubeColor();
 
         if (cubeMaterialRef.current) {
             cubeMaterialRef.current.color.lerp(targetColor, 0.1);
+        }
+
+        if (moveCubeRigidBodyRef.current) {
+            if (movement.current.w) direction.z -= speed;
+            if (movement.current.s) direction.z += speed;
+            if (movement.current.a) direction.x -= speed;
+            if (movement.current.d) direction.x += speed;
+            moveCubeRigidBodyRef.current.setNextKinematicTranslation(direction);
         }
     });
 
@@ -51,7 +114,7 @@ function RapierContext() {
         <Suspense>
             <ambientLight intensity={0.2}/>
             <pointLight
-                position={[-2, 2, -1]}
+                position={[2, 2, 1]}
                 castShadow
                 color={[0.3, 0.9, 0.25]}
                 intensity={10.0}
@@ -66,13 +129,17 @@ function RapierContext() {
                     type="kinematicPosition"
                     colliders="cuboid"
                     onCollisionEnter={({other}) => {
-                        if(other.rigidBodyObject?.name === "cubeRigid") cubeCollisionRef.current = true
+                        if (other.rigidBodyObject?.name === "cubeRigid") cubeCollisionRef.current = true
                     }}
                     onCollisionExit={({other}) => {
-                        if(other.rigidBodyObject?.name === "cubeRigid") cubeCollisionRef.current = false
+                        if (other.rigidBodyObject?.name === "cubeRigid") cubeCollisionRef.current = false
                     }}
                 >
-                    <mesh ref={moveCubeMeshRef}>
+                    <mesh
+                        ref={moveCubeMeshRef}
+                        receiveShadow
+                        castShadow
+                    >
                         <boxGeometry args={[0.5, 0.5, 0.5]}/>
                         <meshPhysicalMaterial color={[1.0, 0.5, 0.35]}/>
                     </mesh>
@@ -112,7 +179,7 @@ function RapierContext() {
 
 export default function RapierScene() {
     return (
-        <Canvas camera={{position: [-3, 4, -3]}} shadows>
+        <Canvas camera={{position: [3, 4, 3]}} shadows>
             <RapierContext/>
         </Canvas>
     )
